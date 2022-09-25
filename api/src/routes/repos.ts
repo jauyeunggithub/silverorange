3b807eb8f9b85a5ab70a1c529093cc0a63d521a0
Ho Hang Au Yeung <mayeung@telus.net>
2022-09-25 15:16:09 -0700

    add response

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,13 @@
+import axios from 'axios';
 import { Router, Request, Response } from 'express';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
+  const { data } = await axios.get(
+    'https://api.github.com/users/silverorange/repos'
+  );
   res.header('Cache-Control', 'no-store');
-
   res.status(200);
-
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.json(data);
 });
